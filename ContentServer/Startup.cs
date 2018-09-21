41ebb6ba640a8be82d2b1e7d42d15c9d5387bde6
Nathan Northcutt <nathan@telefrek.com>
2018-09-20 21:12:24 -0700

    content server should work now after i add the PV

--- ContentServer/Startup.cs
+++ ContentServer/Startup.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.FileProviders;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
@@ -40,7 +43,19 @@ namespace ContentServer
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            var cachePeriod = 600;
+
+            app.UseStaticFiles(new StaticFileOptions
+            {
+                FileProvider = new PhysicalFileProvider(
+                    Path.Combine(Directory.GetCurrentDirectory(), Configuration.GetValue("static_directory", "adcontent"))),
+                RequestPath = "/AdContent",
+                OnPrepareResponse = ctx =>
+                {
+                    ctx.Context.Response.Headers.Append("Cache-Control", $"public, max-age={cachePeriod}");
+                }
+            });
+
             app.UseMvc();
         }
     }
