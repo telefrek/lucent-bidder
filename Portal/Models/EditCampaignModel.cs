a242c55e121aff0766f7f726e8695570e9a2f7ad
Nathan Northcutt <nathan@telefrek.com>
2018-11-11 20:51:51 -0800

    getting creatives bound to campaigns

--- Portal/Models/EditCampaignModel.cs
+++ Portal/Models/EditCampaignModel.cs
@@ -12,19 +12,23 @@ using Lucent.Common.Messaging;
 using Microsoft.Extensions.Logging;
 using System.Linq;
 using Lucent.Portal.Entities;
+using System.Collections.Generic;
 
 namespace Lucent.Portal.Models
 {
     public class EditCampaignModel : PageModel
     {
-        private readonly IBasicStorageRepository<Campaign> _db;
+        private readonly IBasicStorageRepository<Campaign> _campaignDb;
+
+        private readonly IBasicStorageRepository<Creative> _creativeDb;
         private readonly ILogger _log;
         private readonly ICampaignUpdateContext _context;
         private readonly IMessageFactory _factory;
 
         public EditCampaignModel(IStorageManager db, ILogger<CreateCampaignModel> log, ICampaignUpdateContext context, IMessageFactory factory)
         {
-            _db = db.GetBasicRepository<Campaign>();
+            _campaignDb = db.GetBasicRepository<Campaign>();
+            _creativeDb = db.GetBasicRepository<Creative>();
             _log = log;
             _context = context;
             _factory = factory;
@@ -33,12 +37,16 @@ namespace Lucent.Portal.Models
         [BindProperty]
         public Campaign Campaign { get; set; }
 
+        [BindProperty]
+        public List<Creative> CreativeCatalog { get; set; }
+
         public string FilterTypeValue { get; set; }
 
         public async Task<IActionResult> OnGetAsync(string id)
         {
             _log.LogInformation("Geting campaign {id}", id);
-            var c = await _db.Get(id);
+            var c = await _campaignDb.Get(id);
+            CreativeCatalog = (await _creativeDb.GetAll()).ToList();
 
             if (c != null)
             {
@@ -49,6 +57,48 @@ namespace Lucent.Portal.Models
             return RedirectToPage("./Index");
         }
 
+        public async Task<IActionResult> OnPostRemoveCreativeAsync(string id)
+        {
+            _log.LogInformation("Removing creative {id}", id);
+            var creative = await _creativeDb.Get(id);
+            CreativeCatalog = (await _creativeDb.GetAll()).ToList();
+            if(creative != null)
+            {
+                Campaign.Creatives.Remove(creative);
+                _log.LogInformation("Updating campaign");
+                if(await _campaignDb.TryUpdate(Campaign))
+                {
+                    _log.LogInformation("Success");
+                    Campaign = await _campaignDb.Get(Campaign.Id);
+                }
+                else
+                    _log.LogWarning("Failed to update campaign");
+            }
+
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAddCreativeAsync(string id)
+        {
+            _log.LogInformation("Adding creative {id}", id);
+            var creative = await _creativeDb.Get(id);
+            CreativeCatalog = (await _creativeDb.GetAll()).ToList();
+            if(creative != null)
+            {
+                Campaign.Creatives.Add(creative);
+                _log.LogInformation("Updating campaign");
+                if(await _campaignDb.TryUpdate(Campaign))
+                {
+                    _log.LogInformation("Success");
+                    Campaign = await _campaignDb.Get(Campaign.Id);
+                }
+                else
+                    _log.LogWarning("Failed to update campaign");
+            }
+
+            return Page();
+        }
+
         public async Task<IActionResult> OnPostAsync()
         {
             _log.LogInformation("Validating post");
@@ -57,12 +107,12 @@ namespace Lucent.Portal.Models
                 return Page();
             }
 
-            var c = await _db.Get(Campaign.Id);
+            var c = await _campaignDb.Get(Campaign.Id);
             if (c != null)
             {
                 try
                 {
-                    if (await _db.TryUpdate(Campaign))
+                    if (await _campaignDb.TryUpdate(Campaign))
                     {
                         await _context.UpdateCampaignAsync(Campaign, CancellationToken.None);
 
